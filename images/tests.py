2dd0dc43489b060ae74d975c665e17925e626f58
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-05 14:22:56 +0300

    add location test setup method

--- images/tests.py
+++ images/tests.py
@@ -2,3 +2,7 @@ from django.test import TestCase
 from .models import Image, Category, Location
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.nairobi = Location(name= 'Nairobi')
