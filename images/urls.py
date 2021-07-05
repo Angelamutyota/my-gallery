ca828e0575a32d7dce480d48ddc8cf3bdae525b6
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-05 13:45:03 +0300

    add bootstrap styling to image display

--- images/urls.py
+++ images/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.welcome,name = 'index'),
     url(r'^search/', views.search_image_results, name='search_image_results'),
-    url(r'^image/(\d+)',views.image, name ='image')
+    url(r'^image/(\d+)',views.image, name ='image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
