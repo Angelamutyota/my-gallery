6fbf6cd7bd21900206aa3ab3b16a5a7c99f4c4ae
ANGELA <angela.mbithe@student.moringaschool.com>
2021-07-06 14:43:29 +0300

    add save image to image test class

--- images/tests.py
+++ images/tests.py
@@ -25,5 +25,9 @@ class ImageTestClass(TestCase):
         self.nature.save_category()
 
 
+        self.new_image= Image(image='image.jpg', name = 'imagetest', desc= 'this is a test', loc= self.nairobi, category= self.nature)
+        self.new_image.save()
+
+
     
       
